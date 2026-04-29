9d8aa7cada9a51a63e76b6e6f3d41387a72ee16d
Borshcherin0 <mobzdubob@gmail.com>
2026-04-29 14:13:08 +0300

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,1 +1,11 @@
+import os
+import psycopg
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise RuntimeError("DATABASE_URL не задан!")
+
+
+def get_db():
+    """Создаёт подключение к PostgreSQL"""
+    return psycopg.connect(DATABASE_URL)
