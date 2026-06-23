a7447628d51fa5eb1aafaa4c1052deaf4cbf5a12
Borshcherin0 <mobzdubob@gmail.com>
2026-06-23 14:09:12 +0300

    Update ui.js

--- static/js/ui.js
+++ static/js/ui.js
@@ -48,7 +48,6 @@ async function switchPanel(name) {
     if (name === 'ingredients') await loadIngredients();
     if (name === 'inventory') await loadInventory();
     if (name === 'events') await loadEventsAdmin();
-    if (name === 'tournaments') await loadTournaments();
 }
 
 function updateSelects() {
